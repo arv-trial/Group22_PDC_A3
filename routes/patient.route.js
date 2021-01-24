589fd3bc7997ecf3073edf9cc6b2665aec9953c3
GiangNguyen <giang.nguyen@skedulo.com>
2021-01-24 22:31:58 +0700

    update POST /patient response data

--- routes/patient.route.js
+++ routes/patient.route.js
@@ -9,55 +9,47 @@ app.get("/:id", (req, res, next) => {
     "SELECT * FROM patient WHERE insurance_id = ? ",
     [id],
     (err, rows, fields) => {
-      if (err)
-        next(err)
-      console.log('rows', rows)
+      if (err) next(err);
+      console.log("rows", rows);
       if (rows.length) {
-
         const result = {
           ...rows[0],
           id: parseInt(rows[0].insurance_id.slice(3)),
         };
-        console.log('result', result)
+        console.log("result", result);
         // res.header("Access-Control-Expose-Headers", "Content-Range");
         // res.header("Content-Range", "bytes : 0-9/*");
         return res.status(200).json(result);
       }
       return res.status(404).json({
-        message: 'Item not found'
-      })
+        message: "Item not found",
+      });
     }
   );
 });
 
 app.put("/:edit_id", function (req, res, next) {
   let insurance_id = "iid" + req.params["edit_id"];
-  const {
-    id,
-    ...rest
-  } = req.body
+  const { id, ...rest } = req.body;
 
   connection.query(
-    "UPDATE `patient` SET ? WHERE insurance_id = ?", [rest, insurance_id],
+    "UPDATE `patient` SET ? WHERE insurance_id = ?",
+    [rest, insurance_id],
     function (error, results, fields) {
-      if (error)
-        next(error)
+      if (error) next(error);
       else {
-        const {
-          affectedRows
-        } = results
+        const { affectedRows } = results;
         if (affectedRows)
           return res.status(200).json({
             data: {
               id: parseInt(req.params["edit_id"]),
-              ...req.body
-            }
-          })
+              ...req.body,
+            },
+          });
         return res.status(404).json({
-          message: "Item not found"
-        })
-
-      };
+          message: "Item not found",
+        });
+      }
     }
   );
 });
@@ -65,31 +57,27 @@ app.put("/:edit_id", function (req, res, next) {
 app.post("/", (req, res, next) => {
   console.log("Trying to create a new user...");
   console.log("How do we get the data?");
-  const {
-    id,
-    ...rest
-  } = req.body
+  const { id, ...rest } = req.body;
   const body = {
     ...rest,
-    insurance_id: "iid" + req.body.id
-  }
+    insurance_id: "iid" + req.body.id,
+  };
   const queryString = "INSERT INTO `patient` SET ?";
-  connection.query(
-    queryString,
-    [body],
-    (err, result, fields) => {
-      if (err)
-        next(err)
-      else
-        return res.status(200).json(result);
-    }
-  );
+  connection.query(queryString, [body], (err, result, fields) => {
+    if (err) next(err);
+    else
+      return res.status(200).json({
+        data: {
+          id: req.body.id,
+          ...result,
+        },
+      });
+  });
 });
 
 app.get("/", (req, res, next) => {
   connection.query("SELECT * FROM patient", (err, rows, fields) => {
-    if (err)
-      next(err)
+    if (err) next(err);
     console.log("Thanh cong");
     console.log("rows", rows);
     const result = rows.map((row) => ({
@@ -116,23 +104,20 @@ app.delete("/:delete_id", (req, res, next) => {
     WHERE clinical_trial.insurance_id = ?`,
     [id],
     (err, rows, fields) => {
-      if (err)
-        next(err)
+      if (err) next(err);
 
       connection.query(
         "DELETE FROM clinical_trial WHERE insurance_id =  ?",
         [id],
         (err, rows, fields) => {
-          if (err)
-            next(err)
+          if (err) next(err);
           console.log("rows", rows);
           if (!err) {
             connection.query(
               "DELETE FROM patient WHERE insurance_id =  ?",
               [id],
               (err, rows, fields) => {
-                if (err)
-                  next(err)
+                if (err) next(err);
                 console.log("Success");
 
                 // res.header("Access-Control-Expose-Headers", "Content-Range");
